870ab089346d0f7c7dfc28e659d3605cbe469abd
MonilDiwan0206 <monil0206@gmail.com>
2019-10-14 11:55:15 +0530

    Update

--- app/src/main/java/com/example/pocketbook/CenterActivity.java
+++ app/src/main/java/com/example/pocketbook/CenterActivity.java
@@ -1,41 +1,65 @@
 package com.example.pocketbook;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import org.w3c.dom.Text;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Objects;
 
 public class CenterActivity extends AppCompatActivity {
 
     private TextView textView;
     private TextView textView1;
-    private TextView textView2;
-    private TextView textView3;
+    private ImageButton imageButton;
     private Button button;
     private DatabaseReference mRef;
     private FirebaseAuth mAuth;
     private DatabaseReference familyRefrence;
     private ProgressDialog mProgress;
+    private StorageReference storageReference;
+    private Button upload;
+    private static final int CAMERA_REQUEST_CODE = 1;
+    private ProgressDialog mProgress1;
+    private Uri uri = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +72,9 @@ public class CenterActivity extends AppCompatActivity {
         mRef = FirebaseDatabase.getInstance().getReference();
         mAuth = FirebaseAuth.getInstance();
         familyRefrence = FirebaseDatabase.getInstance().getReference().child("Family");
-
+        upload = findViewById(R.id.uploadbtn);
+        storageReference = FirebaseStorage.getInstance().getReference();
+        mProgress1 = new ProgressDialog(this);
 
         final String uid = mAuth.getCurrentUser().getUid();
         final String date = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
@@ -56,6 +82,16 @@ public class CenterActivity extends AppCompatActivity {
         Bundle bundle = getIntent().getExtras();
         final String message = bundle.getString("position");
         mProgress = new ProgressDialog(this);
+
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
+                }
+            }
+        });
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -99,5 +135,45 @@ public class CenterActivity extends AppCompatActivity {
                 });
             }
         });
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
+
+            mProgress1.setMessage("Uploading Image...");
+            mProgress1.show();
+
+
+
+            Bundle extra = data.getExtras();
+            Bitmap bitmap = (Bitmap) extra.get("data");
+            //uri = (Uri) extra.get("data");
+            uri = getImageUri(bitmap);
+
+
+            StorageReference filepath = storageReference.child("photos").child(uri.getLastPathSegment());
+            filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    mProgress1.dismiss();
+                    Toast.makeText(CenterActivity.this, "Uploading successfully....", Toast.LENGTH_LONG).show();
+                }
+            });
+
+
+        }
+
+    }
+
+    private Uri getImageUri(Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
     }
 }
